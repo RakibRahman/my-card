965fb6e42b052b1d31c6af066d738585fd3fc3c1
RakibRahman <md.rr.talukder@gmail.com>
2021-12-06 15:42:14 +0600

    init 90%

--- src/context/context.js
+++ src/context/context.js
@@ -11,18 +11,16 @@ const initState = {
   selectedCard: null,
   cardInfo: null,
   count: 0,
+  lastCardInfo: {},
 };
 const reducer = (state, action) => {
   switch (action.type) {
-    case "reload_data": {
-      return state;
-    }
     case "add_card": {
       const info = {
         id: nanoid(),
         title: `demo card ${state.count}`,
         description: `This is card ${state.count}`,
-        bgColor: "",
+        bgColor: "#000000",
       };
       return { ...state, card: [...state.card, info], count: state.count++ };
     }
@@ -31,18 +29,23 @@ const reducer = (state, action) => {
       const index = action.payload;
       return { ...state, cardInfo: state.card[index], selectedCard: index };
     }
-
+    case "last_card_info": {
+      return {
+        ...state,
+        lastCardInfo: state.card[state.card.length - 1],
+      };
+    }
     case "add_new_card": {
       const selectedIndex = action.payload;
       const addAfter = (array, index, newItem) => {
         return [...array.slice(0, index), newItem, ...array.slice(index)];
       };
+
       const newArray = addAfter(state.card, selectedIndex + 1, {
         id: nanoid(),
         title: `demo card ${state.count}`,
-
         description: `This is card ${state.count}`,
-        bgColor: "",
+        bgColor: "#000000",
       });
       return {
         ...state,
@@ -60,6 +63,7 @@ const reducer = (state, action) => {
         card: newArr,
       };
     }
+
     default:
       return state;
   }
